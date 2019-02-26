416cf5a17a393baa545365c1cc79a28ef17bca2b
Robert Tessnow-Ramirez <tessnowr@gmail.com>
2019-02-25 21:20:21 -0500

    Commented Out C+++ code

--- 99_bottles_beer.py
+++ 99_bottles_beer.py
@@ -26,24 +26,24 @@ while beers >-1:
             print(beers," bottle"," of beer on the wall,",beers," bottle",s," of beer.", sep='')
             beers -= 1
 
-// C++ version of 99 Bottles of beer
-// programmer: Tim Robinson timtroyr@ionet.net
-// Corrections by Johannes Tevessen
+# // C++ version of 99 Bottles of beer
+# // programmer: Tim Robinson timtroyr@ionet.net
+# // Corrections by Johannes Tevessen
 
-#include <iostream>
-using namespace std;
-
-int main()
-    {
-    int bottles = 99;
-    while ( bottles > 0 )
-        {
-        cout << bottles << " bottle(s) of beer on the wall," << endl;
-        cout << bottles << " bottle(s) of beer." << endl;
-        cout << "Take one down, pass it around," << endl;
-        cout << --bottles << " bottle(s) of beer on the wall." << endl;
-        }
-    return 0;
-    }
+# #include <iostream>
+# using namespace std;
+#
+# int main()
+#     {
+#     int bottles = 99;
+#     while ( bottles > 0 )
+#         {
+#         cout << bottles << " bottle(s) of beer on the wall," << endl;
+#         cout << bottles << " bottle(s) of beer." << endl;
+#         cout << "Take one down, pass it around," << endl;
+#         cout << --bottles << " bottle(s) of beer on the wall." << endl;
+#         }
+#     return 0;
+#     }
 
 
